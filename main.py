b3f2edcc27c5d2d0c60f65e893c57aaa4b796c83
Cathy Wu <cathywu@alum.mit.edu>
2015-02-01 16:17:05 -0800

    experiment 2 results

--- main.py
+++ main.py
@@ -189,7 +189,7 @@ def experiment(m=2,fname=None):
     num_delays = 10
     spreadlist = [0, 0.05, 0.1]
     inertialist = [0, 0.05, 0.1, 0.2, 0.3]
-    balancinglist = [0, 0.001, 0.002, 0.003, 0.004]
+    balancinglist = [0, 0.0005, 0.001, 0.002, 0.003]
 
     # spreadlist = [0]
     # inertialist = [0]
